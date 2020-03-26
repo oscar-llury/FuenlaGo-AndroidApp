49ed3b5b0373be5024048186fb0fceecdfadd118
oscar <oscar.rivas.99@gmail.com>
2020-03-26 13:29:23 +0100

    added ocultar ayuda to settings

--- app/src/main/java/com/example/fuenlago/Settings.java
+++ app/src/main/java/com/example/fuenlago/Settings.java
@@ -2,7 +2,7 @@ package com.example.fuenlago;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
+import com.example.fuenlago.ayuda;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -21,7 +21,7 @@ public class Settings extends AppCompatActivity {
     private ImageButton ButtonEventos;
     private ImageButton ButtonNoticias;
 
-    private boolean ayuda = false;
+    private boolean ayudaCheck = false;
     private ImageButton ayudaButton;
     private ConstraintLayout help1;
     private ConstraintLayout help2;
@@ -29,6 +29,8 @@ public class Settings extends AppCompatActivity {
     private ConstraintLayout help4;
     private ConstraintLayout help5;
     private ConstraintLayout help6;
+    private ayuda ayuda;
+    private Button ocultarAyuda;
 
     private Button restablecer;
     private Switch daltonico;
@@ -44,6 +46,8 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        ayuda = new ayuda();
+
         restablecer= findViewById(R.id.restablecer);
         daltonico= findViewById(R.id.switchDaltonico);
         oscuro= findViewById(R.id.switchOscuro);
@@ -51,6 +55,13 @@ public class Settings extends AppCompatActivity {
         mediano= findViewById(R.id.radioButtonM);
         grande= findViewById(R.id.radioButtonG);
         volumen= findViewById(R.id.volumen);
+        ocultarAyuda= findViewById(R.id.ocultarAyuda);
+        help1 = findViewById(R.id.help1);
+        help2 = findViewById(R.id.help2);
+        help3 = findViewById(R.id.help3);
+        help4 = findViewById(R.id.help4);
+        help5 = findViewById(R.id.help5);
+        help6 = findViewById(R.id.help6);
 
         ButtonProfile= findViewById(R.id.Perfil);
         ButtonProfile.setOnClickListener(new View.OnClickListener() {
@@ -90,18 +101,37 @@ public class Settings extends AppCompatActivity {
         });
 
         ayudaButton = findViewById(R.id.ayudaButton);
+        if(!ayuda.isShowAyuda()){
+            ayudaButton.setVisibility(View.GONE);
+        }
+        ocultarAyuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ayuda.isShowAyuda()){
+                    ocultarAyuda.setText("Mostrar ayuda");
+                    ayudaButton.setVisibility(View.GONE);
+                    ayudaCheck = false;
+                    help1.setVisibility(View.GONE);
+                    help2.setVisibility(View.GONE);
+                    help3.setVisibility(View.GONE);
+                    help4.setVisibility(View.GONE);
+                    help5.setVisibility(View.GONE);
+                    help6.setVisibility(View.GONE);
+                    ayuda.setShowAyuda(false);
+                }else {
+                    ocultarAyuda.setText("Ocultar ayuda");
+                    ayudaButton.setVisibility(View.VISIBLE);
+                    ayuda.setShowAyuda(true);
+                }
+            }
+        });
+
         ayudaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                help1 = findViewById(R.id.help1);
-                help2 = findViewById(R.id.help2);
-                help3 = findViewById(R.id.help3);
-                help4 = findViewById(R.id.help4);
-                help5 = findViewById(R.id.help5);
-                help6 = findViewById(R.id.help6);
 
-                if (ayuda) {
-                    ayuda = false;
+                if (ayudaCheck) {
+                    ayudaCheck = false;
                     help1.setVisibility(View.GONE);
                     help2.setVisibility(View.GONE);
                     help3.setVisibility(View.GONE);
@@ -109,7 +139,7 @@ public class Settings extends AppCompatActivity {
                     help5.setVisibility(View.GONE);
                     help6.setVisibility(View.GONE);
                 } else {
-                    ayuda = true;
+                    ayudaCheck = true;
                     help1.setVisibility(View.VISIBLE);
                     help2.setVisibility(View.VISIBLE);
                     help3.setVisibility(View.VISIBLE);
---
+++ app/src/main/java/com/example/fuenlago/ayuda.java
@@ -0,0 +1,18 @@
+package com.example.fuenlago;
+
+public class ayuda {
+
+    private boolean showAyuda;
+
+    public ayuda() {
+        this.showAyuda = true;
+    }
+
+    public boolean isShowAyuda() {
+        return showAyuda;
+    }
+
+    public void setShowAyuda(boolean showAyuda) {
+        this.showAyuda = showAyuda;
+    }
+}
